1e0cfd0609dc15e0ca99f9e262e78044b45e90e2
Jaya <jayasankhla50020712@gmail.com>
2026-07-16 18:15:25 +0530

    pair sum vector

---
+++ bvector6.cpp
@@ -0,0 +1,33 @@
+//Pair Sum (vector)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> pairsum(vector<int> arr, int target) {
+    int start =0, end= arr.size()-1;
+    int currsum =0;
+    vector<int> ans;
+    while (start <end) {
+        currsum = arr[start] + arr[end];
+        if (currsum == target) {
+            ans.push_back(start);
+            ans.push_back(end);
+            return ans;
+        }
+        else if (currsum > target) {
+            end--;
+        }
+        else {
+            start++;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> vec={2,7,11,15};
+    int target = 9;
+    vector<int> ans = pairsum(vec,target);
+    cout <<ans[0]<<","<<ans[1]<<endl;
+    return 0;
+}
