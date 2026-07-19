388b042665a86afd82fdf050fba65a1dff2e0f98
Jaya <jayasankhla50020712@gmail.com>
2026-07-20 00:53:53 +0530

    binary to decimal

---
+++ cnumbersys2.cpp
@@ -0,0 +1,19 @@
+//Binary to Decimal
+#include <iostream>
+using namespace std;
+void bintodec(int binnum) {
+    int n = binnum;
+    int decnum =0;
+    int pow = 1;
+    while (n >0)  {
+        int lastdig = n % 10;
+        decnum = decnum + lastdig * pow;
+        pow = pow *2;
+        n = n/10;
+    }
+    cout <<decnum<<endl;
+}
+int main() {
+    bintodec(101);
+    return 0;
+}
