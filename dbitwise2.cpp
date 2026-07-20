0dfe0b8c3f237ebffd9d3921fce27a87dfe96c33
Jaya <jayasankhla50020712@gmail.com>
2026-07-20 23:02:16 +0530

    binary not operator

---
+++ dbitwise2.cpp
@@ -0,0 +1,10 @@
+//Ones's Complement (Binary NOT Operator)
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<~0<<endl;
+    cout<<~6<<endl;
+    cout<<~10<<endl;
+    cout<<~2<<endl;
+    return 0;
+}
