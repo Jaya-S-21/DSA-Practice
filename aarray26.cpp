252f5c6a0db3d9f9f4cede08137a5899d27781d7
Jaya <jayasankhla50020712@gmail.com>
2026-07-26 22:34:53 +0530

    squares of sorted array (easy)

---
+++ aarray26.cpp
@@ -0,0 +1,19 @@
+//Squares of sorted array -> easy approach
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<int> sortedSquares(vector<int>& nums) {
+    for (int i=0; i<nums.size(); i++) {
+        nums[i] *= nums[i];
+    }
+    sort(nums.begin(), nums.end());
+    return nums;
+}
+int main() {
+    vector<int> nums= {-4,-1,0,2,6,10};
+    vector <int> ans = sortedSquares(nums);
+    for (int i=0; i<ans.size(); i++) {
+        cout<< ans[i]<<" ";
+    }
+}
