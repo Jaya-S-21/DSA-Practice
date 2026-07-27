b2f3de9ea217626b4525b747bba33fd5f0e07510
Jaya <jayasankhla50020712@gmail.com>
2026-07-27 23:22:31 +0530

    product except self -> optimised

--- aarray25.cpp
+++ aarray25.cpp
@@ -1,25 +1,21 @@
 //Product of Array except Self
-//ans = leftproduct * rightproduct -> brute force
+//ans = leftproduct * rightproduct -> optimised
 #include <iostream>
 #include <vector>
 using namespace std;
 vector<int> productExceptself(vector<int>& nums) {
     int n = nums.size();
     vector<int> answer(n);
-    vector<int> rightprod(n);
-    rightprod[n-1] = 1;
     answer[0] = 1;
     for (int i=1; i<n; i++) {
         answer[i] = answer[i-1] * nums[i-1];
     }
-    for (int i=n-2; i>=0; i--) {
-        rightprod[i] = rightprod[i+1] * nums[i+1];
+    int right = 1;
+    for (int i=n-1; i>=0; i--) {
+        answer[i] = answer[i] * right;
+        right = right * nums[i];
     }
-    for (int i=0; i<n; i++) {
-        answer[i] = answer[i] * rightprod[i];
-        nums[i] = answer[i];
-    }
-    return nums;
+    return answer;
 }
 int main() {
     vector<int> nums = {1,2,3,4};
