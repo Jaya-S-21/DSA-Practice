4b7342f849639dfcefe2c4455949f1ea29f81d7a
Jaya <jayasankhla50020712@gmail.com>
2026-07-31 00:44:48 +0530

    squares of sorted arrar -> 2 pointer

--- aarray26.cpp
+++ aarray26.cpp
@@ -1,14 +1,25 @@
-//Squares of sorted array -> easy approach
+//Squares of sorted array -> 2 pointer
 #include <iostream>
 #include <algorithm>
 #include <vector>
 using namespace std;
 vector<int> sortedSquares(vector<int>& nums) {
-    for (int i=0; i<nums.size(); i++) {
-        nums[i] *= nums[i];
-    }
-    sort(nums.begin(), nums.end());
-    return nums;
+    vector<int> ans(nums.size());
+    int start = 0, end = nums.size() -1;
+    int k = end;
+    while (start <= end) {
+        int startsq = nums[start] * nums[start];
+        int endsq = nums[end] * nums[end];
+        if (startsq > endsq) {
+            ans[k] = startsq;
+            start++;
+        } else {
+            ans[k] = endsq;
+            end--;
+        }
+        k--;
+    }        
+    return ans;
 }
 int main() {
     vector<int> nums= {-4,-1,0,2,6,10};
