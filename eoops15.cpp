a57d8230570904ec5fdea5ff3c8c46e6363340e8
Jaya <jayasankhla50020712@gmail.com>
2026-08-01 00:38:08 +0530

    static variables & objects

--- eoops15.cpp
+++ eoops15.cpp
@@ -1,4 +1,4 @@
-//Static Variable
+//Static Variable & Static Object
 #include <iostream>
 using namespace std;
 void counter() {
@@ -12,6 +12,16 @@ class ccounter{
 };
 int ccounter::x =0;
 
+class example{
+    public:
+    example() {
+        cout<<"constructor..."<<endl;
+    }
+    ~example() {
+        cout<<"destructor..."<<endl;
+    }
+};
+
 int main() {
     counter();        //1
     counter();        //2
@@ -21,5 +31,11 @@ int main() {
     cout<<c1.x++<<endl;         //0
     cout<<c2.x++<<endl;         //1
     cout<<c3.x++<<endl;         //2
+
+    int x=0;
+    if(x==0) {
+        example e1;                 // or static example e1
+    }
+    cout<<"code ending....."<<endl;
     return 0;
 }
