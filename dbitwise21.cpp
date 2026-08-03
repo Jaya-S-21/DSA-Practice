08523ad97d2bde87e571697d4443dbdfc431fea2
Jaya <jayasankhla50020712@gmail.com>
2026-08-04 01:53:51 +0530

    bitwise AND of number range

---
+++ dbitwise21.cpp
@@ -0,0 +1,16 @@
+//Bitwise AND of number range
+#include <iostream>
+using namespace std;
+int rangeBitwiseAnd(int left, int right) {
+    int count =0 ;
+    while (left != right) {
+        left= left >> 1;
+        right = right >> 1;
+        count ++ ;
+    }
+    return (left << count);
+}
+int main() {
+    cout <<rangeBitwiseAnd(5,7)<<endl;
+    return 0;
+}
