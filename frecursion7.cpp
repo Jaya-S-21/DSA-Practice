ff3bfd66be45b348829a6909527c46be51a55fb7
Jaya <jayasankhla50020712@gmail.com>
2026-08-06 23:47:38 +0530

    first occurence

---
+++ frecursion7.cpp
@@ -0,0 +1,19 @@
+//First Occurence
+#include <iostream>
+#include <vector> 
+using namespace std;
+int firstOccurence(vector<int>& nums, int i, int target) {
+    if (nums[i] == target) {
+        return i;
+    }
+    if (nums[i] == nums.size()) {
+        return -1;
+    }
+    return firstOccurence(nums,i+1,target);
+}
+using namespace std;
+int main() {
+    vector<int> nums= {1,2,3,3,3,4};
+    cout<<firstOccurence(nums, 0, 3) <<endl;
+    return 0;
+}
