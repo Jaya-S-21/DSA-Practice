0783764e0a594ba8531aba63570f1e641bb04b74
Jaya <jayasankhla50020712@gmail.com>
2026-08-10 02:34:45 +0530

    last occurence

---
+++ frecursion8.cpp
@@ -0,0 +1,19 @@
+//Last Occurence
+#include <iostream>
+#include <vector>
+using namespace std;
+int lastOccurence(vector<int>& nums, int i, int target) {
+    if (i == nums.size()) {
+        return -1;
+    }
+    int lastindex = lastOccurence(nums,i+1,target);
+    if (lastindex == -1 && nums[i] == target) {
+        return i;
+    }
+    return lastindex;
+}
+int main() {
+    vector<int> nums= {1,2,3,3,3,4};
+    cout << lastOccurence(nums, 0, 3) <<endl;
+    return 0;
+}
