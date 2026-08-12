0cb4ab3e33ece2fb833da66c3c5e966c40e7aed1
Jaya <jayasankhla50020712@gmail.com>
2026-08-13 02:03:31 +0530

    remove duplicates from string using recursion

---
+++ frecursion11.cpp
@@ -0,0 +1,25 @@
+//Remove Duplicates from String using recursion
+#include <iostream>
+#include <string>
+using namespace std;
+void removeDuplicate(string str, string ans, int i, int map[26]) {
+    if (i == str.size()) {
+        cout << "ans: "<< ans <<endl;
+        return;
+    }
+    char ch = str[i];
+    int mapIdx = (int)(ch - 'a');
+    if (map[mapIdx]) { //duplicate
+        removeDuplicate(str,ans,i+1,map);
+    } else { //non-duplicate
+        map[mapIdx] = true;
+        removeDuplicate(str,ans+str[i],i+1,map);
+    }
+}
+int main() {
+    string str = "appnnacollege";
+    string ans = "";
+    int map [26] = {false};
+    removeDuplicate(str, ans,0, map);
+    return 0;
+}
