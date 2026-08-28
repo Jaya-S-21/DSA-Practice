0115127abafd1c25e13fa2cc16bd4a522974a84d
Jaya <jayasankhla50020712@gmail.com>
2026-08-29 02:08:22 +0530

    convert to lowercase

---
+++ hchar3.cpp
@@ -0,0 +1,20 @@
+//Convert to Lowercase 
+#include <iostream>
+#include <cstring>
+using namespace std;
+void toLower(char word[], int n) {
+    for (int i=0 ; i <n; i++) {
+        char ch = word[i];
+        if (ch >= 'a' && ch <= 'z') {  //lowercase
+            continue;
+        } else {                       //uppercase
+            word[i] = ch - 'A' + 'a';
+        }
+    }
+}
+int main() {
+    char word[] = "JAYASANKHLA";
+    toLower(word, strlen(word));
+    cout <<word<<endl;
+    return 0;
+}
