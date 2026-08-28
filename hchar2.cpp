bebcf23d4dc112f3268720ba2840feee66156f8f
Jaya <jayasankhla50020712@gmail.com>
2026-08-29 01:27:43 +0530

    convert to uppercase

---
+++ hchar2.cpp
@@ -0,0 +1,19 @@
+//Convert to Uppercase 
+#include <iostream>
+#include <cstring>
+using namespace std;
+void toUpper(char word[], int n) {
+    for (int i=0 ; i <n; i++) {
+        char ch = word[i];
+        if (ch >= 'A' && ch <= 'Z') {  //uppercase
+            continue;
+        } else {                       //lowercase
+            word[i] = ch - 'a' + 'A';
+        }
+    }
+}
+int main() {
+    char word[] = "jayasankhla";
+    toUpper(word, strlen(word));
+    cout <<word<<endl;
+}
