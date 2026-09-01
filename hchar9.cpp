1c852e99b4b7c7a077a7d3f33eddfc478fe4107e
Jaya <jayasankhla50020712@gmail.com>
2026-09-01 23:12:54 +0530

    no of lowercase character

--- hchar9.cpp
+++ hchar9.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int countlower(string str) {
     int count =0;
     for (int i=0; i <str.length(); i++) {
-        if (str[i] >= 'a' && str[i] <='z') {
+        if (str[i] == 'a' || str[i] =='e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') {
             count++;
         }
     }
