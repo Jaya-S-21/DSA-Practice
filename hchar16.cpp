d866d1ff8d7fe0689e8b2502ba6c37ae3382cd6f
Jaya <jayasankhla50020712@gmail.com>
2026-09-07 01:05:39 +0530

    repeated substring pattern

---
+++ hchar16.cpp
@@ -0,0 +1,29 @@
+//Repeated Substring Pattern
+#include <iostream>
+#include <cstring>
+using namespace std;
+bool repeatedSubstringPattern(string s) {
+    int n= s.length(); 
+    for (int i=1; i<=n/2; i++) {
+        bool repeated = true;
+        if (n % i == 0) {
+            for (int j= i; j <n; j++) {
+                if (s[j] != s[j % i]) {
+                    repeated = false;
+                    break;
+                }
+            }
+            if (repeated) return true;
+        }
+    }
+    return false;
+}
+int main() {
+    string s = "abab";
+    cout<<repeatedSubstringPattern(s)<<endl;
+    string s2 = "abcabcabc";
+    cout<<repeatedSubstringPattern(s2)<<endl;
+    string s3 = "abcab";
+    cout<<repeatedSubstringPattern(s3)<<endl;
+    return 0;
+}
