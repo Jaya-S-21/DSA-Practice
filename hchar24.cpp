09f6519631b95acdeb95ec14c635372035a9d165
Jaya <jayasankhla50020712@gmail.com>
2026-09-20 03:20:14 +0530

    reverse only letters

---
+++ hchar24.cpp
@@ -0,0 +1,23 @@
+//Reverse only letters
+#include <iostream>
+#include <algorithm>
+using namespace std;
+string reverseOnlyLetters(string s) {
+    int i =0;
+    int j = s.length() - 1;
+    while(i <= j) {
+        if (isalpha(s[i]) && isalpha(s[j])) {
+            swap(s[i++], s[j--]);
+        }else if (!isalpha(s[i])) {
+            i++;
+        }else {
+            j--;
+        }
+    }
+    return s;
+}
+int main() {
+    string s = "Test1ng-Leet=code-Q!";
+    cout << reverseOnlyLetters(s)<<endl;
+    return 0;
+}
